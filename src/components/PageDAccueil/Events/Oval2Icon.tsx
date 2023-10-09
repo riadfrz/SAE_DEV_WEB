64671134727c8dbb85341730f5099526b80b53d7
TMTVato <128924487+TMTVato@users.noreply.github.com>
2023-10-09 21:33:33 +0200

    Delete src directory

--- src/components/PageDAccueil/Events/Oval2Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Oval2Icon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 68 69' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <ellipse cx={33.9011} cy={34.1209} rx={33.9011} ry={34.1209} fill='#6BC6B8' fillOpacity={0.8} />
-  </svg>
-);
-const Memo = memo(Oval2Icon);
-export { Memo as Oval2Icon };
