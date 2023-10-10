5d544e8be019b68e644e1455f2ca57c6b4b0e149
riadfrz <riadfarouzi@gmail.com>
2023-10-10 11:21:44 +0200

    ,

---
+++ src/Pages/Presentation.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/navbar/Navbar";
+import Hero from "../components/hero/Hero";
+import Actu from "../components/actu/Actu";
+import Agenda from "../components/agenda/Agenda";
+import React from "react";
+
+function Presentation() {
+    return (
+        <div>
+
+            <Navbar />
+        </div>
+    );
+}
+
+export default Presentation;
