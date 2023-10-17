3f76a53943bfb089a3327b424bac72b3930dd85f
riadfrz <riadfarouzi@gmail.com>
2023-10-17 09:22:56 +0200

    x

--- src/Pages/Presentation.js
+++ src/Pages/Presentation.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
 import Pres from "../components/pres_code/pres";
+
 import React from "react";
 
 function Presentation() {
--- src/components/pres_code/pres.js
+++ src/components/pres_code/pres.js
@@ -14,32 +14,70 @@ const Presentation = () => {
             <div className={classes.image_processing20221115238301}>
                 <div className={classes.presentation}>Présentation</div>
             </div>
-            
-            {/*<div className={classes.rectangle14}></div>*/}
-            {/*<div className={classes.presentation}>Présentation</div>*/}
-            <div className={classes.textBlock}>Qui Somme nous?</div>
-            <div className={classes.test}>
+
+
+            <div className={classes.morlaixCommunauteEtablissement}>
+                <div className={classes.textBlock}>Qui Somme nous?</div>
+                <div className={classes.rectangle}></div>
+                <div className={classes.test}>
+                    Morlaix Communauté, Établissement Public de Coopération Intercommunale (EPCI), a été fondé le 14 avril 1995 et
+                    regroupe actuellement 26 communes.
+                </div>
+                <div className={classes.test}>
+                    <p></p>
+                </div>
+                <div className={classes.test}>
+                    Grâce à l&#39;exercice de ses diverses compétences, Morlaix Communauté s&#39;engage résolument dans une
+                    mission de service public visant à améliorer la qualité de vie de ses résidents, en mettant en œuvre une série
+                    de projets concrets.
+                </div>
+            </div>
+            <div className={classes.leConseilDeCommunaute}>
+                <div className={classes.textBlock}>Le conseil de communauté</div>
+                <div className={classes.rectangle}></div>
+                <div className={classes.test}>
+                    <p></p>
+                </div>
+                <div className={classes.morlaixCommunauteComposeeDe26C}>
+                    <div className={classes.test}>
+                        <p></p>
+                    </div>
+                    <div className={classes.test}>
+                        Morlaix Communauté, composée de 26 communes membres, est représentée par un Conseil de Communauté composé de
+                        51 conseillers communautaires. Le nombre de conseillers par commune est déterminé en fonction de la
+                        population.
+                    </div>
+                    <div className={classes.test}>
+                        <p></p>
+                    </div>
+                    <div className={classes.test}>
+                        Le Conseil de Communauté exerce la gestion et l&#39;administration de la communauté d&#39;agglomération par le
+                        biais de ses délibérations. Il a la possibilité de déléguer certaines de ses compétences au Président et au
+                        Bureau communautaire. Lors de chaque séance du conseil, le Président informe l&#39;assemblée des décisions
+                        prises par délégation.
+                    </div>
+                    <div className={classes.test}>
+                        <p></p>
+                    </div>
+                    <div className={classes.test}>
+                        Les séances du conseil se tiennent à intervalles réguliers, généralement toutes les deux mois. Découvrez
+                        comment le Conseil de Communauté de Morlaix Communauté contribue à la gestion intercommunale et à
+                        l&#39;amélioration de la vie de ses habitants.
+                    </div>
+                </div>
+            </div>
+            <div className={classes.depuisLe12Avril2021LeBureauCom}>
+                <div className={classes.textBlock}>Le bureau communautaire</div>
+                <div className={classes.rectangle}></div>
+                <div className={classes.test}>
                     Depuis le 12 avril 2021, le Bureau Communautaire de Morlaix Communauté a connu une évolution majeure.
                     Initialement composé du président, des 11 vice-présidents, des 10 conseillers délégués et d&#39;un conseiller
                     spécial, il a été étendu pour inclure les maires des 26 communes n&#39;occupant pas ces fonctions.
                 </div>
-            <div className={classes.leConseilDeCommunaute}>
-                <div className={classes.textBlock}>Le conseil de communauté</div>
                 <div className={classes.test}>
                     <p></p>
                 </div>
-            </div>
-            <div className={classes.lesCommissions}>
-                <div className={classes.textBlock}>Les commissions</div>
-                <div className={classes.test}>
-                    <p></p>
-                </div>
-            </div>
-            <div className={classes.depuisLe12Avril2021LeBureauCom}>
-                <div className={classes.test}>
-                    <p></p>
-                </div>
-               
+
                 <div className={classes.test}>
                     <p></p>
                 </div>
@@ -58,48 +96,16 @@ const Presentation = () => {
                     dans la gestion de Morlaix Communauté.&quot;
                 </div>
             </div>
-            <div className={classes.leBureauCommunautaire}>Le bureau communautaire</div>
-            <div className={classes.morlaixCommunauteEtablissement}>
-                <div className={classes.test}>
-                    Morlaix Communauté, Établissement Public de Coopération Intercommunale (EPCI), a été fondé le 14 avril 1995 et
-                    regroupe actuellement 26 communes.
-                </div>
+
+
+            <div className={classes.lesCommissions}>
+                <div className={classes.textBlock}>Les commissions</div>
+                <div className={classes.rectangle}></div>
                 <div className={classes.test}>
                     <p></p>
-                </div>
-                <div className={classes.test}>
-                    Grâce à l&#39;exercice de ses diverses compétences, Morlaix Communauté s&#39;engage résolument dans une
-                    mission de service public visant à améliorer la qualité de vie de ses résidents, en mettant en œuvre une série
-                    de projets concrets.
                 </div>
             </div>
-            <div className={classes.morlaixCommunauteComposeeDe26C}>
-                <div className={classes.test}>
-                    <p></p>
-                </div>
-                <div className={classes.test}>
-                    Morlaix Communauté, composée de 26 communes membres, est représentée par un Conseil de Communauté composé de
-                    51 conseillers communautaires. Le nombre de conseillers par commune est déterminé en fonction de la
-                    population.
-                </div>
-                <div className={classes.test}>
-                    <p></p>
-                </div>
-                <div className={classes.test}>
-                    Le Conseil de Communauté exerce la gestion et l&#39;administration de la communauté d&#39;agglomération par le
-                    biais de ses délibérations. Il a la possibilité de déléguer certaines de ses compétences au Président et au
-                    Bureau communautaire. Lors de chaque séance du conseil, le Président informe l&#39;assemblée des décisions
-                    prises par délégation.
-                </div>
-                <div className={classes.test}>
-                    <p></p>
-                </div>
-                <div className={classes.test}>
-                    Les séances du conseil se tiennent à intervalles réguliers, généralement toutes les deux mois. Découvrez
-                    comment le Conseil de Communauté de Morlaix Communauté contribue à la gestion intercommunale et à
-                    l&#39;amélioration de la vie de ses habitants.
-                </div>
-            </div>
+
             <div className={classes.lesCommissionsDeMorlaixCommuna}>
                 <div className={classes.test}>
                     <p></p>
@@ -118,11 +124,8 @@ const Presentation = () => {
                     instances délibératives, jouant ainsi un rôle essentiel dans le processus décisionnel de Morlaix Communauté.
                 </div>
             </div>
-            <div className={classes.rectangle20}></div>
-            <div className={classes.rectangle24}></div>
-            <div className={classes.rectangle22}></div>
-            <div className={classes.rectangle23}></div>
-     </div>    
+
+     </div>
     )
     
 };
