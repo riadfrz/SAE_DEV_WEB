cfb850e89c399afccedd5e69b9f11e608f19ff3c
Ahovar <90898432+Ahovar@users.noreply.github.com>
2023-10-19 15:27:34 +0200

    header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Hero.module.css';
+import { AiOutlineSearch } from 'react-icons/ai';
+
+const Header = () => {
+  return (
+    <div className={styles.hero}>
+      <h1>ETUDIER A MORLAIX</h1>
+    </div>
+  );
+};
+
+export default Header;
